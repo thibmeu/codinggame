6912a0769a076d58fb176095ebe284bd9aef68fc
Thibault Meunier <wognovice@gmail.com>
2018-04-21 19:57:11 +0100

    Code Royale: build valid

--- Challenge/Code Royale/main.py
+++ Challenge/Code Royale/main.py
@@ -6,6 +6,12 @@ from enum import Enum
 # the standard input according to the problem statement.
 
 
+class console:
+    @staticmethod
+    def log(message):
+        print(message, file=sys.stderr)
+
+
 class Position:
     MIN_X = 0
     MIN_Y = 0
@@ -25,18 +31,63 @@ class Position:
         return self._y
 
     def __str__(self):
-        return '({}, {})'.format(self.x, self.y)
+        return '{} {}'.format(self.x, self.y)
 
     def _sanitize(self):
-        self.x = min(max(self.x, MIN_X), MAX_X)
-        self.y = min(max(self.y, MIN_Y), MAX_Y)
+        self.x = min(max(self.x, Position.MIN_X), Position.MAX_X)
+        self.y = min(max(self.y, Position.MIN_Y), Position.MAX_Y)
 
-    def move(_direction, _range = 1):
-        if _direction in Direction:
-            self.__addition(_direction, _range)
+    def add(self, _position):
+        self._x += _position.x
+        self._y += _position.y
+
+    def sub(self, _position):
+        self._x -= _position.x
+        self._y -= _position.y
+
+    def mult(self, _scale):
+        self._x *= _scale
+        self._y *= _scale
 
     def distance(self, _position):
         return math.sqrt((self._x - _position.x)**2 + (self._y - _position.y)**2)
+
+    def round(self):
+        self._x = round(self._x)
+        self._y = round(self._y)
+
+    def move(self, _position, _max_distance):
+        if self.distance(_position) <= _max_distance:
+            return _position
+        ab = Position(_position.x, _position.y)
+        ab.sub(self)
+        console.log('Reine: {}, Site: {}, Step: {}'.format(self, _position, ab))
+        ab.mult(_max_distance / self.distance(_position))
+        console.log('Reine: {}, Site: {}, Step: {}'.format(self, _position, ab))
+        ab.add(self)
+        ab.round()
+        console.log('Reine: {}, Site: {}, Step: {}'.format(self, _position, ab))
+        return ab
+
+
+class Circle:
+    def __init__(self, _centre, _radius):
+        self.centre = Position(_centre.x, _centre.y)
+        self.radius = _radius
+
+    def distance(self, _circle):
+        return self.centre.distance(_circle.centre) - (self.radius + _circle.radius)
+
+    def touch(self, _circle):
+        return self.distance(_circle) == 0
+
+    def borders(self):
+        return [
+            Position(self.centre.x + self.radius, self.centre.y),
+            Position(self.centre.x - self.radius, self.centre.y),
+            Position(self.centre.x, self.centre.y + self.radius),
+            Position(self.centre.x, self.centre.y - self.radius)
+        ]
 
 
 class OwnerType(Enum):
@@ -60,17 +111,22 @@ class Unit:
 
 
 class Queen(Unit):
+    radius = 30
+    maxDistance = 60
+
     def __init__(self, *kwargs):
-        super().__init__(kwargs)
-        self.gold = 0
-        self.touchSite = -1
+        super().__init__(*kwargs)
+        self.gold = kwargs[5]
+        self.touchSite = kwargs[6]
+
+    def to_circle(self):
+        return Circle(self.position, Queen.radius)
 
 
-class Site:
+class Site(Circle):
     def __init__(self, *kwargs):
+        super().__init__(Position(kwargs[1], kwargs[2]), kwargs[3])
         self.id = kwargs[0]
-        self.position = Position(kwargs[1], kwargs[2])
-        self.radius = kwargs[3]
 
 
 class StructureType(Enum):
@@ -86,13 +142,50 @@ class ProductionType(Enum):
 class Structure(Site):
     def __init__(self, *kwargs):
         site = kwargs[0]
-        super().__init__(site.id, site.position.x, site.position.y, site.radius)
+        super().__init__(site.id, site.centre.x, site.centre.y, site.radius)
         self.ignored1 = kwargs[1]
         self.ignored2 = kwargs[2]
         self.structureType = StructureType(kwargs[3])
         self.owner = OwnerType(kwargs[4])
         self.timeout = kwargs[5]
         self.production = kwargs[6]
+
+
+class Methods:
+    @staticmethod
+    def performQueenAction(queen, structures):
+        action1 = True
+        print('---- {}'.format(queen.touchSite), file=sys.stderr)
+        if queen.touchSite != -1:
+            site = structures[queen.touchSite]
+            if site.structureType == StructureType.EMPTY:
+                print('BUILD {} BARRACKS-KNIGHT'.format(site.id))
+                action1 = False
+
+        if action1:
+            closest_site = Methods.closest_site(queen, structures)
+            console.log('--------------------- DISTAAAAAAAAAAAANCE {}'.format(queen.to_circle().distance(closest_site)))
+            if closest_site:
+                step = queen.position.move(closest_site.centre, queen.maxDistance)
+                print('MOVE {} {}'.format(step.x, step.y))
+            else:
+                print('WAIT')
+
+    @staticmethod
+    def closest_site(queen, sites):
+        site_distances = []
+        for key in sites:
+            site = sites[key]
+            site_distances.append({'site_id': site.id, 'distance': site.distance(queen.to_circle())})
+
+        site_distances.sort(key=(lambda x: x['distance']))
+        for site in site_distances:
+            if sites[site['site_id']].structureType == StructureType.EMPTY \
+                    or sites[site['site_id']].owner == OwnerType.ENEMY:
+                return sites[site['site_id']]
+
+        # Happens if we control every site
+        return None
 
 
 # Beginning of the main method
@@ -120,7 +213,7 @@ while True:
     num_units = int(input())
     for i in range(num_units):
         x, y, owner, unit_type, health = [int(j) for j in input().split()]
-        if unit_type == UnitType.QUEEN and owner == OwnerType.ALLY:
+        if unit_type == UnitType.QUEEN.value and owner == OwnerType.ALLY.value:
             queen = Queen(x, y, owner, unit_type, health, gold, touched_site)
         else:
             units.append(Unit(x, y, owner, unit_type, health))
@@ -131,5 +224,6 @@ while True:
 
     # First line: A valid queen action
     # Second line: A set of training instructions
-    print('WAIT')
+    Methods.performQueenAction(queen, structures)
+
     print('TRAIN')
